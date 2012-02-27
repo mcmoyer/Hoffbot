76a47e6259fac7be2725aea4eb48b351d3a6e26c
mcmoyer <marlon@mcmoyer.com>
2012-02-27 05:55:02 -0600

    Update insults.js

--- insults.js
+++ insults.js
@@ -1,10 +1,10 @@
 var insults = [
-	"You must have been born on a highway because that's where most accidents happen.",
+  "You must have been born on a highway because that's where most accidents happen.",
   "I'd like to see things from your point of view but I can't seem to get my head that far up my ass.",
-	"For a man with no cows, you've got an awful lot of bullshit.",
-	"You're so ugly, when you were born your mom said \"What a treasure\" and your dad said \"Yea lets go bury it\"!",
-	"Did the aliens forget to remove your anal probe?",
-	"Whats the difference between your girlfriend and a walrus?\n\nOne has a moustache and smells of fish and the other is a walrus.",
+  "For a man with no cows, you've got an awful lot of bullshit.",
+  "You're so ugly, when you were born your mom said \"What a treasure\" and your dad said \"Yea lets go bury it\"!",
+  "Did the aliens forget to remove your anal probe?",
+  "Whats the difference between your girlfriend and a walrus?\n\nOne has a moustache and smells of fish and the other is a walrus.",
   "Your mother was a hamster and your father smelt of elderberries."
 ]	
 
